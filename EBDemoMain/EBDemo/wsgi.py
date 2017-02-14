67ae2200cf0915af3b74230032d3098b5ebffc03
nabohall <nabohall1224@icloud.com>
2017-02-13 20:54:37 -0800

    Adds python 3 shebang

--- EBDemoMain/EBDemo/wsgi.py
+++ EBDemoMain/EBDemo/wsgi.py
@@ -6,11 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
-
+#!/usr/bin/python3
 import os
 
 from django.core.wsgi import get_wsgi_application
-raise RuntimeError("WSGI working directory: {}".format(os.getcwd()))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EBDemo.settings")
 
